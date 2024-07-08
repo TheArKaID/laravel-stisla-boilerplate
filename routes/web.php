55b4a4ace08abdc46cfc2ff9df79fd5c4db522f8
Arifia Kasastra R <arka.progammer@gmail.com>
2024-07-08 12:36:21 +0700

    Removing Blank Page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.blank-page');
+    return redirect()->route('login');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
