b6a520d9c593505dffc11c173b4b5a30e19d8275
Arifia Kasastra R <arka.progammer@gmail.com>
2024-07-08 14:22:54 +0700

    chore: Add logout functionality to header component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -267,10 +267,13 @@
                     <i class="fas fa-cog"></i> Settings
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#"
-                    class="dropdown-item has-icon text-danger">
+                <a href="#" class="dropdown-item has-icon text-danger"
+                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
